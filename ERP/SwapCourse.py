8cc44fd62950156aeedc0c7d6b8c7a3969a261ff
revanthmopidevi <revanthmopidevi@gmail.com>
2020-08-18 11:05:33 +0530

    update

--- ERP/SwapCourse.py
+++ ERP/SwapCourse.py
@@ -5,51 +5,66 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 import time
 from selenium.common.exceptions import NoSuchElementException
 
+def main(flag = False):
+        # https://sites.google.com/a/chromium.org/chromedriver/downloads
+        PATH = "C:\\chromedriver.exe"
+        driver = webdriver.Chrome(PATH)
+        #___________________________________________________________________#
+        erp_login_link = "https://sis.erp.bits-pilani.ac.in/psp/sisprd/?cmd=login"
+        #___________________________________________________________________#
+        erp_swap_link = ""
+        #___________________________________________________________________#
+        userID, password = "41120170280", "password"
+        #___________________________________________________________________#
+        toDrop = "HSS F244: CRIME AND NEW MEDIA"
+        #___________________________________________________________________#
+        toPick = "F372"
+        #___________________________________________________________________#
+        while not flag:
+                login(erp_login_link, userID, password)
+                flag = swap(erp_swap_link,toDrop, toPick)     
+
+def login(erp_login_link, userID, password):
+        driver.get(erp_login_link)
+        driver.find_element_by_xpath("""//*[@id="userid"]""").send_keys(userID)
+        driver.find_element_by_xpath("""//*[@id="pwd"]""").send_keys(password)
+        driver.find_element_by_xpath("""/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr[1]/td/table[2]/tbody/tr[4]/td[3]/input""").click()
+
+def swap(erp_swap_link, toDrop, toPick):
+        driver.get(erp_swap_link)
+        driver.switch_to.frame(driver.find_element_by_xpath("""//*[@id="ptifrmtgtframe"]"""))
+        try:
+                elem=element1 = driver.find_element_by_name("DERIVED_REGFRM1_DESCR50$37$")
+                if elem.is_displayed():
+                        drp1 = Select(element1)
+                        drp1.select_by_visible_text(toDrop)
+                        driver.find_element_by_xpath("""//*[@id="DERIVED_REGFRM1_SSR_PB_SRCH$41$"]""").click()
+                        time.sleep(1)
+                        driver.find_element_by_name("CLASS_SRCH_WRK2_SUBJECT$64$")
+                        # select course discipline (CS, EEE, ECE, INSTR, FIN, ECON etc.) by changing option[num]
+                        driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SUBJECT$64$"]/option[6]""").click()
+                        time.sleep(1)
+                        driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_CATALOG_NBR$72$"]""").send_keys(toPick)
+                        driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH"]""").click()
+                        time.sleep(1)
+                        try:
+                                elem = driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SSR_PB_SELECT$0"]""")
+                                if elem.is_displayed():
+                                        elem.click()
+                                        time.sleep(1)
+                                        driver.find_element_by_xpath("""//*[@id="DERIVED_CLS_DTL_NEXT_PB$75$"]""").click()
+                                        time.sleep(1)
+                                        driver.find_element_by_xpath("""//*[@id="DERIVED_REGFRM1_SSR_PB_SUBMIT"]""").click()
+                                        return True
+                        except NoSuchElementException:
+                                driver.close()
+                                return False
+        except NoSuchElementException:
+                driver.close()
+                return False
+
 if __name__ == "__main__":
-        while True:
-                # webdriver version == google chrome's version
-                # https://sites.google.com/a/chromium.org/chromedriver/downloads
-                driver = webdriver.Chrome('C:\\chromedriver.exe')
-                # ERP login page 
-                driver.get('https://sis.erp.bits-pilani.ac.in/psp/sisprd/?cmd=login')
-                driver.find_element_by_xpath("""//*[@id="userid"]""").send_keys("41120170280")
-                driver.find_element_by_xpath("""//*[@id="pwd"]""").send_keys("password")
-                driver.find_element_by_xpath("""/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr[1]/td/table[2]/tbody/tr[4]/td[3]/input""").click()
-                # ERP swap page
-                driver.get("")
-                driver.switch_to.frame(driver.find_element_by_xpath("""//*[@id="ptifrmtgtframe"]"""))
-                try:
-                        elem=element1 = driver.find_element_by_name("DERIVED_REGFRM1_DESCR50$37$")
-                        if elem.is_displayed():
-                                drp1 = Select(element1)
-                                drp1.select_by_visible_text("HSS F244: CRIME AND NEW MEDIA")
-                                driver.find_element_by_xpath("""//*[@id="DERIVED_REGFRM1_SSR_PB_SRCH$41$"]""").click()
-                                time.sleep(1)
-                                driver.find_element_by_name("CLASS_SRCH_WRK2_SUBJECT$64$")
-                                # select course discipline (CS, EEE, ECE, INSTR, FIN, ECON etc.) by changing option[num]
-                                driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SUBJECT$64$"]/option[6]""").click()
-                                time.sleep(1)
-                                # course code
-                                driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_CATALOG_NBR$72$"]""").send_keys("F372")
-                                driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH"]""").click()
-                                time.sleep(1)
-                                try:
-                                        elem = driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SSR_PB_SELECT$0"]""")
-                                        if elem.is_displayed():
-                                                # click the element if present
-                                                elem.click()
-                                                time.sleep(1)
-                                                driver.find_element_by_xpath("""//*[@id="DERIVED_CLS_DTL_NEXT_PB$75$"]""").click()
-                                                time.sleep(1)
-                                                driver.find_element_by_xpath("""//*[@id="DERIVED_REGFRM1_SSR_PB_SUBMIT"]""").click()
-                                                print("\nSWAP SUCCESFUL.\n")
-                                                break
-                                except NoSuchElementException:
-                                    driver.close()
-                except NoSuchElementException:
-                        driver.close()
-
-
+        main()
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # elems = driver.find_element_by_xpath("""//*[@id="CLASS_SRCH_WRK2_SSR_PB_SELECT$0"]""")
 
